65188be05e49f15760827dc7a2552ac584e2e8d7
phvinayak017 <vinayak.hulabutti@gmail.com>
2019-08-12 23:20:42 +0530

    Updates

---
+++ precache-manifest.e018adbd7ada648a412c6cc8fd0d1fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bc14e512fee49fa09a6ddfea019097",
+    "url": "/React-Projects/index.html"
+  },
+  {
+    "revision": "eaaab74c1a19b833a1e9",
+    "url": "/React-Projects/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ce44f00b4cdb9f653b81",
+    "url": "/React-Projects/static/js/2.cbfaaa39.chunk.js"
+  },
+  {
+    "revision": "eaaab74c1a19b833a1e9",
+    "url": "/React-Projects/static/js/main.53634fac.chunk.js"
+  },
+  {
+    "revision": "16917cc2d26703b07e0e",
+    "url": "/React-Projects/static/js/runtime~main.52070902.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
